09ecda391f8e059d97c19888d0b4ad62a7393000
eggrah <eggrah@mail.ru>
2022-05-27 15:55:50 +0300

    1

--- test_items.py
+++ test_items.py
@@ -2,9 +2,9 @@ import time
 
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+lin = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_check_button_add_to_basket(browser):
-    browser.get(link)
+    browser.get(lin)
     assert browser.find_element(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]') is not None
