f38fbc3d019c5651f55840f9f655ccc04dae3cb9
eggrah <eggrah@mail.ru>
2022-05-28 14:16:15 +0300

    added: should_be_login_url should_be_login_form should_be_register_form

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 # def test_guest_can_go_to_login_page(browser):
@@ -9,7 +10,7 @@ from .pages.main_page import MainPage
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_page()
