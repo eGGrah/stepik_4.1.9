a52365aecc740544e3194a568d6ad8eeb8d455d5
eggrah <eggrah@mail.ru>
2022-05-31 19:16:29 +0300

    fin

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,7 @@ from .pages.product_page import ProductPage
 
 
 @pytest.mark.login_user
-class TestUserAddToBasketFromProductPage():
+class TestUserAddToBasketFromProductPage:
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -19,6 +19,7 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(email=str(time.time()) + "@fakemail.org", password='qwerty228')
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
         page = ProductPage(browser, link)
@@ -31,9 +32,10 @@ class TestUserAddToBasketFromProductPage():
         link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
         page = ProductPage(browser, link)
         page.open()
-        assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), 'a'
+        assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), 'element present'
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
@@ -54,6 +56,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -61,6 +64,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
     page = BasketPage(browser, link)
